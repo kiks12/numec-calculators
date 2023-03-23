52e1e291fb800b8bb6abe0ae6709ae71c5640dea
Francis James Tolentino <francistolentino1107@gmail.com>
2023-03-23 18:42:38 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("public/index.html");
+})
+
+
+app.listen(PORT, () => console.log(`NUMERICAL METHOD CALCULATOR: Running at port ${PORT}`));
+
